330b0370fd87ef062048032a2a1d3ada28af6dea
Angelica <angelicacb6392@gmail.com>
2025-03-19 11:36:52 -0500

    NUEWVI

--- Backend/DatabaseConnection/DatabaseConnection.cs
+++ Backend/DatabaseConnection/DatabaseConnection.cs
@@ -9,7 +9,7 @@ namespace Huellero.Backend.DatabaseConnection
 {
     class DatabaseConnection
     {
-        private readonly string _connectionString = "Host=10.30.1.238;Port=5432;Database=Register;Username=postgres;Password=Admin";
+        private readonly string _connectionString = "Host=10.30.1.238;Port=5432;Database=RegisterAsistencia;Username=postgres;Password=Admin";
         public async Task<NpgsqlConnection> GetConnectionAsync()
         {
             var connection = new NpgsqlConnection(_connectionString);
